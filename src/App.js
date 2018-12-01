0cf64343dcf3f6fc3564d2b8d2be566506382a26
Rett Walker <rettwalker@gmail.com>
2018-12-01 14:17:13 -0500

    adding package and call

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
+import { AuthenticateUser } from './services/authentication';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
+        <Login {submitLogin: this.submitLogin} />
       </div>
     );
+  }
+
+  submitLogin = (user) => {
+    return AuthenticateUser(user).then((res) => { console.log('YESSSS') })
   }
 }
 
