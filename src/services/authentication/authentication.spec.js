75f8fd15cd0b4c50f912e171d6f6b04ee71fd326
arthurCollard <collard_aj@yahoo.com>
2018-12-01 13:59:12 -0500

    Added the Authenticate service

---
+++ src/services/authentication/authentication.spec.js
@@ -0,0 +1,33 @@
+import {AuthenticateUserRequestGenerator} from './index'
+
+describe('tests the authentication', () => {
+    describe('when the user attempts to login', () => {
+        let requestMock
+        let authenicateUser
+        beforeAll( () => {
+            requestMock = jest.fn()
+            authenicateUser = AuthenticateUserRequestGenerator(requestMock)
+        })
+        it('the user information is returned', () => {
+            requestMock.mockResolvedValue({
+                "id": 1,
+                "email": "test@email.com",
+                "first_name": "Test",
+                "last_name": "McTester"
+            })
+            return authenicateUser({email:'',password:''}).then(res => {
+                expect(res).toEqual({
+                    "id": 1,
+                    "email": "test@email.com",
+                    "first_name": "Test",
+                    "last_name": "McTester"
+                })
+                expect(requestMock).toBeCalled()
+                expect(requestMock).toBeCalledWith('localhost:8080/api/login', {method:'POST', body: JSON.stringify({email:'',password:''})})
+            })
+        })
+    }) 
+    it('', () => {
+        expect(AuthenticateUserRequestGenerator()).toBeInstanceOf(Function)
+    })
+})
---
+++ src/services/authentication/index.js
@@ -0,0 +1,9 @@
+
+let AuthenticateUserRequestGenerator = (requestFn) => {
+    return function(user){
+        return requestFn('localhost:8080/api/login',{method:'POST', body: JSON.stringify(user)})
+    }
+}
+
+let AuthenticateUser = AuthenticateUserRequestGenerator(fetch)
+export {AuthenticateUserRequestGenerator, AuthenticateUser}
